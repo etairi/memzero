8e7c71c1d35502d3baa17595b9e6a7b885cfb2c9
Erkan Tairi <erkan.tairi@gmail.com>
2018-10-23 13:39:21 +0200

    Add correctness test

--- src/memzero_bench.c
+++ src/memzero_bench.c
@@ -3,17 +3,36 @@
  * Based on: https://github.com/open-quantum-safe/liboqs/issues/48
  */
 
+#define _GNU_SOURCE
 #define __STDC_WANT_LIB_EXT1__ 1
 #include <assert.h>
+#include <errno.h>
+#include <setjmp.h>
+#include <signal.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "ds_benchmark.h"
 
+// Constants for our benchmarks.
 #define MEMZERO_TEST_SMALL_BLOCK_SIZE   0x1000L
 #define MEMZERO_TEST_LARGE_BLOCK_SIZE   0x1000000L
 #define MEMZERO_BENCH_SECONDS           1
+
+// Number of times to write the secret.
+#define MEMZERO_STACK_SIZE              (SIGSTKSZ + sizeof(secret))
+
+// The secret that we write out to the stack. 
+// After cleaning, we shouldn't be able to find it in our stack.
+static const char secret[24] = {
+	0x4e, 0x65, 0x76, 0x65, 0x72, 0x20, 0x67, 0x6f,
+	0x6e, 0x6e, 0x61, 0x20, 0x67, 0x69, 0x76, 0x65,
+	0x20, 0x79, 0x6f, 0x75, 0x20, 0x75, 0x70, 0x2c,
+};
+
+// Memory allocated for our stack.
+static char stack_buf[MEMZERO_STACK_SIZE];
 
 enum memzero_alg_name {
     memzero_alg_memset,
@@ -53,6 +72,139 @@ memzero_testcase_t memzero_testcases[] = {
     { memzero_alg_SecureZeroMemory, "SecureZeroMemory" }
 #endif
 };
+
+
+// Verify that we are on the custom stack.
+static void assert_on_stack(void) {
+    stack_t current_stack;
+    assert(0 == sigaltstack(NULL, &current_stack));
+    assert(SS_ONSTACK == (current_stack.ss_flags & SS_ONSTACK));
+}
+
+// Call the provided signal handler on a custom stack.
+static void call_on_stack(void (*fn)(int)) {
+    const stack_t stack = {
+        .ss_sp = stack_buf,
+        .ss_size = sizeof(stack_buf),
+    };
+
+    const struct sigaction action = {
+        .sa_handler = fn,
+        .sa_flags = SA_ONSTACK,
+    };
+
+    stack_t old_stack;
+    struct sigaction old_action;
+
+    // Setup the stack and signal handler.
+    assert(0 == sigaltstack(&stack, &old_stack));
+    assert(0 == sigaction(SIGUSR1, &action, &old_action));
+
+    // Raise a signal. This will only return after the signal handler has returned.
+    assert(0 == raise(SIGUSR1));
+
+    // Restore the previous state, disable our alt stack.
+    sigaction(SIGUSR1, &old_action, NULL);
+    sigaltstack(&old_stack, NULL);
+}
+
+/**
+ * Test a provided memory clean algorithm. Must be called from the custom stack.
+ * First writes the secret to the stack, then runs the provided cleaning algorithm.
+ * If no cleaning algorithm is provided, just falls back to using memset.
+ *
+ * Returns the address of where the secret was written. If memory cleaning was successful,
+ * the secret should no longer be readable.
+ */
+static char *memzero_test(memzero_func_t memzero) {
+    char buf[sizeof(secret)];
+    char *result;
+
+    assert_on_stack();
+
+    memcpy(buf, secret, sizeof(secret));
+    result = memmem(stack_buf, MEMZERO_STACK_SIZE, buf, sizeof(buf));
+
+    if (memzero != NULL) {
+        memzero(buf, sizeof(buf));
+    } else {
+        // Fallback to memset. If optimization are enabled, this gets optimized out.
+        memset(buf, 0, sizeof(buf));
+    }
+
+    return result;
+}
+
+/**
+ * Verify the secret is where we expect it to be if things are not zeroed out properly.
+ * This implementation uses memset, which should get optimized out. If optimizations aren't 
+ * enabled, this test is skipped.
+ */
+static int memzero_test_correctness_noclean() {
+#ifdef __OPTIMIZE__
+    char *buf;
+    buf = memzero_test(NULL);
+
+    printf("%-30s", "no clean");
+    if (memcmp(buf, secret, sizeof(secret)) == 0) {
+        // The secret is still present, memset was optmized out (as predicted).
+        printf("Test passed\n");
+        return 1;
+    } else {
+        printf("Test failed\n");
+        return 0;
+    }
+#else
+    printf("%-30s", "no clean");
+    printf("Test skipped (no optimizations)\n");
+    return 1;
+#endif
+}
+
+static int memzero_test_correctness_clean(enum memzero_alg_name alg_name, const char *name) {
+    memzero_func_t memzero = memzero_func(alg_name);
+    if (memzero == NULL) {
+        fprintf(stderr, "Error: memzero is NULL\n");
+        return 0;
+    }
+
+    printf("%-30s", name);
+
+    char *buf = memzero_test(memzero);
+    if (memcmp(buf, secret, sizeof(secret)) != 0) {
+        printf("Test passed\n");
+        return 1;
+    } else {
+        printf("Test failed\n");
+        return 0;
+    }
+}
+
+static void memzero_test_correctness_signal_handler(int arg) {
+    (void)(arg);
+    size_t memzero_testcases_len = sizeof(memzero_testcases) / sizeof(struct memzero_testcase);
+    const char * current_test;
+
+    printf("\n================================================================================\n");
+    printf("Memory Cleaning Correctness Test\n");
+    printf("================================================================================\n");
+
+    memzero_test_correctness_noclean();
+
+    for (size_t i = 0; i < memzero_testcases_len; i++) {
+        current_test = memzero_testcases[i].name;
+        if (memzero_test_correctness_clean(memzero_testcases[i].alg_name, memzero_testcases[i].name) != 1) {
+            goto error;
+        }
+    }
+
+    printf("Success: all memory cleaning correctness tests passed.\n\n");
+    return;
+
+    error:
+        printf("Error: %s failed the memory cleaning correctness test.\n", current_test);
+        return;
+}
 
 typedef void *(*memset_t)(void *, int, size_t);
 static volatile memset_t memset_func = memset;
@@ -138,7 +290,7 @@ memzero_func_t memzero_func(enum memzero_alg_name alg_name) {
     }
 }
 
-static int memzero_test_wrapper(enum memzero_alg_name alg_name, const char *name) {
+static int memzero_bench(enum memzero_alg_name alg_name, const char *name) {
     memzero_func_t memzero = memzero_func(alg_name);
     if (memzero == NULL) {
         fprintf(stderr, "Error: memzero is NULL\n");
@@ -168,11 +320,13 @@ static int memzero_test_wrapper(enum memzero_alg_name alg_name, const char *name
 int main(void) {
     int success;
     const char * current_test;
-
     size_t memzero_testcases_len = sizeof(memzero_testcases) / sizeof(struct memzero_testcase);
+
+    call_on_stack(memzero_test_correctness_signal_handler);
+
     for (size_t i = 0; i < memzero_testcases_len; i++) {
         current_test = memzero_testcases[i].name;
-        success = memzero_test_wrapper(memzero_testcases[i].alg_name, memzero_testcases[i].name);
+        success = memzero_bench(memzero_testcases[i].alg_name, memzero_testcases[i].name);
         if (success != 1) {
             goto error;
         }
